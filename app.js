e59a2180c92205753e9aa9b5f0dc35ff0abee2f6
RJBalaka <rjbalaka@gmail.com>
2026-08-19 23:28:41 +0900

    added the reaction scheduler

--- app.js
+++ app.js
@@ -10,6 +10,22 @@ import {
 } from 'discord-interactions';
 import { getRandomEmoji, DiscordRequest } from './utils.js';
 import { getShuffledOptions, getResult } from './game.js';
+import cron from 'cron';
+
+import {
+  Client,
+  GatewayIntentBits,
+  Events,
+  TimestampStyles
+} from 'discord.js';
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMessageReactions,
+  ]
+});
 
 // Create an express app
 const app = express();
@@ -17,6 +33,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 // To keep track of our active games
 const activeGames = {};
+
+//guild
+let guild;
+let channel;
+let bigWalkRole;
 
 //print time
 const startupNow = new Date();
@@ -28,6 +49,8 @@ console.log('Unixtime: ', unixTime);
  * Interactions endpoint URL where Discord will send HTTP requests
  * Parse request body and verifies incoming requests using discord-interactions package
  */
+
+ //command list
 app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), async function (req, res) {
   // Interaction id, type and data
   const { id, type, data } = req.body;
@@ -210,12 +233,6 @@ if (type === InteractionType.MESSAGE_COMPONENT) {
     }
 }
 
-
-
-
-
-
-
 //error handling
   console.error('unknown interaction type', type);
   return res.status(400).json({
@@ -223,6 +240,119 @@ if (type === InteractionType.MESSAGE_COMPONENT) {
   });
 });
 
+//constructor(cronTime, onTick, onComplete, start, timeZone, context, runOnInit, utcOffset, unrefTimeout, waitForCompletion, errorHandler, name, threshold):
+//repeat message periodically      crontime        
+//                                 onTick (function to execute at time)
+let repeatTime = new cron.CronJob('00 00 9 * * *',
+                                 () => {
+                                    console.log('/repeatTime proc');
+                                    const guild = client.guilds.cache.get('579666389829287936');
+                                    const channel = guild.channels.cache.get('1539140727043522612');
+                                    channel.send('It is 9am');
+                                 },
+                                  null,
+                                  false,
+                                  'Asia/Tokyo'
+                                  );
+//repeatTime.start();
+
+//every day, ping everyone and see when they are available, if everyone is available then ping a possible session time
+
+let dateMessage;
+
+let scheduleRepeat = new cron.CronJob('00 27 23 * * *',
+                                 async () => {
+                                    console.log('/repeatTime proc');
+
+dateMessage = await channel.send(`herro everyone!! <@&${bigWalkRole.id}>
+react 🟥 if youre available from (9am to 12pm) on (tomorrow's date)
+react 🟩 if youre available from (12pm to 4pm) on (tomorrow's date)`);
+                                    await dateMessage.react('🟥');
+                                    await dateMessage.react('🟩');
+
+                                    
+                                 },
+                                  null,
+                                  false,
+                                  'Asia/Tokyo'
+                                  );
+scheduleRepeat.start();
+
+let i = 0;
+let timeSlotFound = false;
+//print current reactions
+let timeSlotArray = [0, 0, 0, 0, 0];
+let reactionCountRepeat = new cron.CronJob('30 * * * * *', 
+                          async() => {
+
+  i = 0;
+  timeSlotArray = [0, 0, 0, 0, 0];
+
+  console.log('/reactionCountRepeat proc');
+
+  if (!dateMessage) {
+    return;
+  }
+
+  let reactionCounts = dateMessage.reactions.cache;
+    for (const reaction of reactionCounts.values()) {
+      console.log('Emoji:', reaction.emoji.name);
+      console.log('Count:', reaction.count);
+          i++;
+    //chatgpt get roles
+      const users = await reaction.users.fetch();
+      for (const user of users.values()) {
+
+        //fetch and print user roles
+        console.log('User:', user.username);
+        const member = await dateMessage.guild.members.fetch(user.id);
+        console.log('Roles:', member.roles.cache.map(role => role.name));
+
+        if (member.roles.cache.has(bigWalkRole.id)) {
+          console.log(`${user.username} is in the Big Walk group, adding to timeslot`);
+          timeSlotArray[i] = timeSlotArray[i] + 1;
+        }
+
+        //if time slot successfully found
+        if (timeSlotArray[i] === bigWalkRole.members.size) {
+
+          console.log('Time slot found at:', reaction.emoji.name);
+
+await channel.send(`<@&${bigWalkRole.id}>
+hoooolly fuck!! An available timeslot was found at this timeslot tomorrow: ${reaction.emoji.name}
+everyone get on big walk tomorrow at this specific time!!`);
+          timeSlotFound = true;
+          
+        }
+      }
+
+    }
+    if (timeSlotFound === true){
+      reactionCountRepeat.stop();
+    }
+  }
+
+);
+reactionCountRepeat.start();
+
+
+//startup
 app.listen(PORT, () => {
   console.log('Listening on port1 bahahahahaha!', PORT);
+});
+
+//chatgpted this part
+client.login(process.env.DISCORD_TOKEN);
+client.once(Events.ClientReady, readyClient => {
+  console.log(`Logged in: ${readyClient.user.tag}`);
+
+  // guild initialization
+guild = client.guilds.cache.get('579666389829287936');
+channel = guild.channels.cache.get('1539140727043522612');
+bigWalkRole = guild.roles.cache.get('1539590284671844422');
+
+console.log('Guild:', guild.name);
+console.log('Channel:', channel.name);
+console.log('Big Walk Role:', bigWalkRole.name);
+
 });
